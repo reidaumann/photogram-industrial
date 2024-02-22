464baf018a1bfc7c3edbe88472c1008b2f8ac444
Reid Aumann <141674197+reidaumann@users.noreply.github.com>
2024-02-22 20:16:26 +0000

    Generated follow requests

--- db/migrate/20240222195552_create_comments.rb
+++ db/migrate/20240222195552_create_comments.rb
@@ -1,7 +1,7 @@
 class CreateComments < ActiveRecord::Migration[7.0]
   def change
     create_table :comments do |t|
-      tt.references :author, null: false, foreign_key: { to_table: :users }, index: false
+      t.references :author, null: false, foreign_key: { to_table: :users }, index: false
       t.references :photo, null: false, foreign_key: true
       t.text :body, null: false
 
