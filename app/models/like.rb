a78ef761a22f7e5ecd5ee387329d9f4ea99b1db7
Reid Aumann <141674197+reidaumann@users.noreply.github.com>
2024-02-22 21:31:45 +0000

    Added sample data rake tasks

--- app/models/like.rb
+++ app/models/like.rb
@@ -21,4 +21,7 @@
 class Like < ApplicationRecord
   belongs_to :fan, class_name: "User", counter_cache: true
   belongs_to :photo, counter_cache: true
+
+  #validation
+  validates :fan_id, uniqueness: { scope: :photo_id, message: "has already liked this photo" }
 end
