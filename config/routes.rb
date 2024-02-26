4f6aa70c31aa1c2bfcb851a81433e94299bc3c01
Reid Aumann <141674197+reidaumann@users.noreply.github.com>
2024-02-26 21:53:25 +0000

    Set up users#show action

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "photos#index"
 
+  get "/:username" => "users#show", as: :user
+
   resources :likes
   resources :follow_requests
   resources :comments
