34585b0f2572d4185e0984bc0e2a54efb3f89734
Reid Aumann <141674197+reidaumann@users.noreply.github.com>
2024-02-26 22:26:32 +0000

    Added links to posts and liked photos on tabbed interface

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "photos#index"
 
+  get ":username/liked" => "users#liked", as: :liked
   get "/:username" => "users#show", as: :user
 
   resources :likes
