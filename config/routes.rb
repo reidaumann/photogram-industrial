8fca4545be3218a87063273935f9ae227a780a75
Reid Aumann <141674197+reidaumann@users.noreply.github.com>
2024-02-26 22:32:44 +0000

    Added views/users/liked.html.erb file

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
   root "photos#index"
 
   get ":username/liked" => "users#liked", as: :liked
+  get ":username/feed"
+  get ":username/followers"
+  get ":username/following"
+  
   get "/:username" => "users#show", as: :user
 
   resources :likes
